4069d3ff840e05c9c3f3a28937cdd83ffd1d57dd
jtrussell <jus.russell@gmail.com>
2014-12-08 22:34:24 -0500

    Add stateless iterator version

--- exercises/07/7-01.lua
+++ exercises/07/7-01.lua
@@ -1,4 +1,5 @@
 
+-- As a closure with state
 function fromto(n, m)
   local ix = n-1
   return function()
@@ -10,7 +11,17 @@ function fromto(n, m)
   end
 end
 
--- Implement as a stateless iterator?
+-- As a stateless iterator
+function fromto_stateless(n, m)
+  local function iter(m, ix)
+    ix = ix + 1
+    if ix > m then
+      return nil
+    end
+    return ix, m
+  end
+  return iter, m, n - 1
+end
 
 io.write('Give me a start number: ')
 local min = io.read('*number')
@@ -18,7 +29,16 @@ local min = io.read('*number')
 io.write('Give me an end number: ')
 local max = io.read('*number')
 
+io.write('Use stateless iterator version (1/0)? ')
+local useStatelss = io.read('*number') == 1
+
 print('Here all the numbers in range (inclusive):')
-for ix in fromto(min, max) do
-  print(ix)
+if(useStatelss) then
+  for ix in fromto_stateless(min, max) do
+    print(ix)
+  end
+else
+  for ix in fromto(min, max) do
+    print(ix)
+  end
 end
