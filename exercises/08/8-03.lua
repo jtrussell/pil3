d8a61e5ced3c475fa714170149ae363a16f69c31
jtrussell <jus.russell@gmail.com>
2014-12-13 23:54:16 -0500

    Add factory for specialized stringrep

--- exercises/08/8-03.lua
+++ exercises/08/8-03.lua
@@ -1,11 +1,28 @@
 
 -- Build an unwrapped stringrep factory.
 function stringrepFactory(n)
-  -- ...
+  local chunk = ''
+  chunk = chunk .. 'local s = ...;local r = "";'
+
+  if n > 0 then
+    while n > 1 do
+      if n % 2 ~= 0 then
+        chunk = chunk .. 'r = r .. s;'
+      end
+      chunk = chunk .. 's = s .. s;'
+      n = math.floor(n/2)
+    end
+    chunk = chunk .. 'r = r .. s;'
+  end
+
+  chunk = chunk .. 'return r'
+
+  return load(chunk)
 end
 
+-- For reference...
 function stringrep(s, n)
-  local r = ""
+  local r = ''
   if n > 0 then
     while n > 1 do
       if n % 2 ~= 0 then r = r .. s end
@@ -16,3 +33,15 @@ function stringrep(s, n)
   end
   return r
 end
+
+io.write('What string should I repeat? ')
+local s = io.read()
+
+io.write('How many times should I repeat it? ')
+local n = io.read('*number')
+
+stringrep_n = stringrepFactory(n)
+local out = stringrep_n(s)
+
+print('Here is what I got:')
+print(out)
