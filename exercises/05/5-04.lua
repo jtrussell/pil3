f766074c7938ce2a398db650128ea0a9757fb698
jtrussell <jus.russell@gmail.com>
2015-01-03 23:38:10 -0500

    Finally cleaned up Ch5 Ex4

--- exercises/05/5-04.lua
+++ exercises/05/5-04.lua
@@ -1,49 +1,63 @@
 
--- @todo not quite done yet... run this code you'll see it misses e.g. "ab"
+-- For reference...
+-- C(n, m) = C(n-1, m-1) + C(n-1, m)
 
--- import `catall`
-dofile '5-01.lua'
+-- Return a table with all 
+function combinations(set, size)
+  local combs = {}
 
--- a, b, c, d
--- a, b, c
--- a, b, d
--- a, c, d
---
--- b, c, d
--- b, c
--- b, d
---
--- c, d
---
--- a
--- b
--- c
--- d
-
-function print_combinations(list)
-  print(catall(table.unpack(list)))
-
-  if #list < 2 then
-    return
+  -- If we don't have enought elements in `set` to build subsets of size `size`
+  -- there's nothing to do
+  if #set < size then
+    return combs
   end
 
-  -- Print subsets containing `h`
-  h = table.remove(list, 1)
-  print_sets(h, list, #list)
+  -- There is only one set with no elements
+  if size == 0 then
+    table.insert(combs, {})
+    return combs
+  end
 
-  -- Now all subsets not containing `h`
-  print_combinations(list)
+  -- We need to snag the first element from `set` and a copy of it without that
+  -- first element
+  local el = set[1]
+  local set2 = {}
+
+  for i = 2, #set do
+    table.insert(set2, set[i])
+  end
+
+  -- C(n-1, m-1)
+  for i, v in ipairs(combinations(set2, size - 1)) do
+    table.insert(v, 1, el)
+    table.insert(combs, v);
+  end
+
+  -- C(n-1, m)
+  for i, v in ipairs(combinations(set2, size)) do
+    table.insert(combs, v)
+  end
+  
+  return combs
 end
 
-function print_sets(h, pool, size)
-  if size == 1 then
-    print(h)
-    return
+function all_combinations(set)
+  local all = {}
+  for size = #set, 1, -1 do
+    for i, v in ipairs(combinations(set, size)) do
+      table.insert(all, v)
+    end
   end
+  return all
+end
 
-  if size < 1 then
-    return
+-- test = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'}
+test = {'a', 'b', 'c', 'd'}
+
+for i, v in ipairs(all_combinations(test)) do
+  if #v == 0 then
+    print('*empty set*')
+  else
+    print(table.unpack(v))
   end
-
-  print_sets(h, pool, size-1)
 end
